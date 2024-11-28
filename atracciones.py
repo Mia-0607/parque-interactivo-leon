4114046046ec97a23e1816d0b7e45bdb2b88b03d
Mia-0607 <194260-7@iberoleon.edu.mx>
2024-11-28 12:01:24 -0600

    E3: Añadir atracciones clásicas

--- atracciones.py
+++ atracciones.py
@@ -1,3 +1,4 @@
+
 # Lista inicial de atracciones en el Parque Interactivo León
 atracciones = [
     'Montaña Rusa',
@@ -5,3 +6,6 @@ atracciones = [
     'Río Lento'
 ]
 atracciones = ['Carros chocones', 'Barco pirata', 'Sillas voladoras']
+
+atracciones_antiguas = ['Rueda de la Fortuna', 'Caballitos'] 
+
